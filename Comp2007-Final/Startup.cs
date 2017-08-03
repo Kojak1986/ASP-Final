da44adfd75f38dcddbcc908d4ca043594db54f3d
Kojak420 <klkovacs86@gmail.com>
2017-08-03 13:59:50 -0400

    Add project files.

---
+++ Comp2007-Final/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Comp2007_Final.Startup))]
+namespace Comp2007_Final
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
