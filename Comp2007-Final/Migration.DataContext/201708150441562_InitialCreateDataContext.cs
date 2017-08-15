8ec3488f513798bf98ca6a3b063f61a1a4358d10
Kojak420 <klkovacs86@gmail.com>
2017-08-15 01:40:25 -0400

    Now Migrations are good!

--- Comp2007-Final/Migration.DataContext/201708150441562_InitialCreateDataContext.cs
+++ Comp2007-Final/Migration.DataContext/201708150441562_InitialCreateDataContext.cs
@@ -57,24 +57,7 @@ namespace Comp2007_Final.Migration.DataContext
                     })
                 .PrimaryKey(t => t.FinishId);
             
-            CreateTable(
-                "dbo.AspNetUsers",
-                c => new
-                    {
-                        Id = c.String(nullable: false, maxLength: 128),
-                        Email = c.String(),
-                        EmailConfirmed = c.Boolean(nullable: false),
-                        PasswordHash = c.String(),
-                        SecurityStamp = c.String(),
-                        PhoneNumber = c.String(),
-                        PhoneNumberConfirmed = c.Boolean(nullable: false),
-                        TwoFactorEnabled = c.Boolean(nullable: false),
-                        LockoutEndDateUtc = c.DateTime(),
-                        LockoutEnabled = c.Boolean(nullable: false),
-                        AccessFailedCount = c.Int(nullable: false),
-                        UserName = c.String(),
-                    })
-                .PrimaryKey(t => t.Id);
+          
             
         }
         
@@ -86,7 +69,6 @@ namespace Comp2007_Final.Migration.DataContext
             DropIndex("dbo.Orders", new[] { "FinishId" });
             DropIndex("dbo.Orders", new[] { "ColourId" });
             DropIndex("dbo.Orders", new[] { "ItemId" });
-            DropTable("dbo.AspNetUsers");
             DropTable("dbo.ItemFinish");
             DropTable("dbo.Items");
             DropTable("dbo.Orders");
